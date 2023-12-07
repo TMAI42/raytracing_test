47781a49a1d5a23161f844dff89a84c7f23c9bd3
tmai42 <andrragon28@gmail.com>
2023-12-07 14:02:11 +0200

    clenup

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,13 @@
 
 int main() {
 
-    Camera* camera = Camera::GetCamera();
-
     // Scene
     Scene scene;
-    scene.AddObject(std::make_shared<Sphere>(Point3D(0,0,-1), 0.5));
+    scene.AddObject(std::make_shared<Sphere>(Point3D (0,0,-1), 0.5));
     scene.AddObject(std::make_shared<Sphere>(Point3D (0,-100.5,-1), 100));
     scene.AddObject(std::make_shared<Sphere>(Point3D (3,0,-3), 2));
 
     // Render
-
-    camera->Render(scene);
+    Camera::GetCamera()->Render(scene);
 
 }
